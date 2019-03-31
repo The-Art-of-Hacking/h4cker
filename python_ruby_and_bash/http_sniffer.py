90bfcbb327ee8c473b8f40acfbf37843562df175
santosomar <santosomar@gmail.com>
2019-03-31 00:17:37 -0400

    creating python sniffers

---
+++ python_ruby_and_bash/http_sniffer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import socket
+
+s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
+
+while True:
+ data=s.recvfrom(65565)
+ try:
+  if "HTTP" in data[0][54:]:
+    print "[","="*30,']'
+    raw=data[0][54:]
+    if "\r\n\r\n" in raw:
+     line=raw.split('\r\n\r\n')[0]
+     print "[*] Header Captured "
+     print line[line.find('HTTP'):]
+    else:
+     print raw
+  else:
+   #print '[{}]'.format(data)
+   pass
+ except:
+  pass
