82e5fca7800a9da8d93f843560d8e73ec4947530
Omar Santos <os@cisco.com>
2021-04-11 19:42:08 -0400

    Create cis_docker-benchmark_inspec_debian_ubuntu.sh

---
+++ docker-and-k8s-security/docker/cis_docker-benchmark_inspec_debian_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A simple script to install Chef inspec and the CIS Docker Benchmark InSpec Profile
+# Author: Omar Santos @santosomar
+
+
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+
