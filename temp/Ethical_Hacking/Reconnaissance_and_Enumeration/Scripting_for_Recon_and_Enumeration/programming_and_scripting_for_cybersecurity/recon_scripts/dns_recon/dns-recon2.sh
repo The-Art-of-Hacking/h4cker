0ec176ba1918537c28ce857a379bdca65aae4c4d
Omar Santos <santosomar@gmail.com>
2026-04-05 16:42:51 -0400

    Delete temp directory

--- temp/Ethical_Hacking/Reconnaissance_and_Enumeration/Scripting_for_Recon_and_Enumeration/programming_and_scripting_for_cybersecurity/recon_scripts/dns_recon/dns-recon2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# bruteforce subdomains
-# Use a wordlist of your choice. I am using dnscan's wordlist in this example
-
-for domain in $(cat /usr/share/wordlists/amass/fierce_hostlist.txt); do host $domain.h4cker.org; done  | grep -v NXDOMAIN | sort -u
